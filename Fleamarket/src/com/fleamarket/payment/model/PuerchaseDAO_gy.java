4ffcb71cf103cfe6b35a63cc147ffda5c53f2560
kosta <kosta@kosta-HP>
2018-08-17 09:52:47 +0900

    2018-08-17 김건엽 payment,마이바티스,구매내역추가,mapper

---
+++ Fleamarket/src/com/fleamarket/payment/model/PuerchaseDAO_gy.java
@@ -0,0 +1,73 @@
+package com.fleamarket.payment.model;
+
+import java.io.InputStream;
+import java.util.List;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import com.fleamarket.mapper.PaymentMapper;
+
+public class PuerchaseDAO_gy {
+	
+	//따로 만든 구매내역 디에이오
+private static PuerchaseDAO_gy dao = new PuerchaseDAO_gy();
+	
+	public static PuerchaseDAO_gy getInstance(){
+		
+		return dao;
+		
+	}
+	
+//sql접속 메소드 입니다.
+	public SqlSessionFactory getSqlSessionFactory(){
+		System.out.println("펙토리안1");
+		
+		String resource = "mybatis-config-payment.xml";
+		System.out.println("펙토리안2");
+		InputStream in = null;
+		
+		try {
+			in = Resources.getResourceAsStream(resource); 
+			System.out.println("펙토리안3");
+			
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("펙토리안4");
+		
+		return new SqlSessionFactoryBuilder().build(in);
+	}
+	
+	// 목록을 가져오는 리메소드 입니다.
+	public List<PuerchaseDTO_gy> listBoard(){
+		
+		System.out.println("리스트 안으로 들어옴1");
+		
+		SqlSession sqlSession = getSqlSessionFactory().openSession();
+		
+		System.out.println("리스트 안으로 들어옴2");
+		List<PuerchaseDTO_gy> list = null;
+		
+		try {
+			System.out.println("리스트 안의 트라이문");
+			
+			list = sqlSession.getMapper(PaymentMapper.class).listPuerchaseBoard(); //mapper 방식 //숫자로 게시판 글 갯수 조정가능
+		//	list = sqlSession.selectList("kosta.mapper.BoardMapper.listBoard"); //id이름뿐만아니라 namespace이름도 갖고와야함
+			
+			System.out.println("리스트 안의 트라이문 종료");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			sqlSession.close();
+		}
+		
+		return list;
+	}
+	
+
+}
